178c50cb419184890e032f090b2996b7619a6628
Isaac Matias Barion <matiasisaac985@gmail.com>
2025-02-02 22:14:16 -0300

    Add files via upload

--- HTML-CSS-JS ALURA/script.js
+++ HTML-CSS-JS ALURA/script.js
@@ -1,25 +1,16 @@
 const searchInput = document.getElementById('search-input');
-const resultArtists = document.getElementById('result-artists');
-const resultPlaylist = document.getElementById('result-playlists');
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlist');
 
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
     fetch(url)
         .then((response) => response.json())
         .then((result) => displayResults(result))
-        .catch((error) => console.error("Erro na requisição:", error));
 }
 
 function displayResults(result) {
-    resultPlaylist.classList.add('hidden');
-    
-    // Verifica se há artistas retornados
-    if (result.length === 0) {
-        resultArtists.innerHTML = "<p>Nenhum artista encontrado.</p>";
-        resultArtists.classList.remove('hidden');
-        return;
-    }
-
+    resultPlaylist.classList.add("hidden")
     const artistName = document.getElementById('artist-name');
     const artistImage = document.getElementById('artist-img');
 
@@ -28,17 +19,16 @@ function displayResults(result) {
         artistImage.src = element.urlImg;
     });
 
-    resultArtists.classList.remove('hidden');
+    resultArtist.classList.remove('hidden');
 }
 
-searchInput.addEventListener('input', function() {
-    const searchTerm = searchInput.value.toLowerCase().trim();
-    
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
     if (searchTerm === '') {
-        resultPlaylist.classList.add('hidden');
-        resultArtists.classList.remove('hidden');
+        resultPlaylist.classList.remove('hidden');
+        resultArtist.classList.add('hidden');
         return;
     }
-
+    
     requestApi(searchTerm);
 });
